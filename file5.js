8303b739eef508928d5bedc37644c4c0b95b4663
IswaryaG1017 <iswaryak1017@gmail.com>
2022-02-14 20:25:08 +0000

    Added few functions compare and merge

--- file5.js
+++ file5.js
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<h1> Displaying Date and Time </h1>
 <h2>My First JavaScript</h2>
 
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.</button>
-
 <p id="demo"></p>
 
+<h3> Thank you for visiting this page</h3>
 </body>
 </html> 
 
